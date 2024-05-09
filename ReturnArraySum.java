0c020c87d48e2514e0346b54a0aa0d54116b203d
Pankaj Singh <p7ksingh@gmail.com>
2024-05-09 23:21:28 +0530

    ReturnArraySum.java answer added

--- ReturnArraySum.java
+++ ReturnArraySum.java
@@ -42,3 +42,20 @@ public class ReturnArraySum {
     }
 
 }
+================================================
+package Array;
+
+public class SumOfArray {
+    public static int sumOfArray(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        System.out.println(sumOfArray(arr));
+    }
+}

f27e2fcc3788f9bb6687b0f5972f32c31416ca8f
Pankaj Singh <p7ksingh@gmail.com>
2024-12-28 22:00:03 +0530

    Add files via upload

---
+++ PrintPattern10.java
@@ -0,0 +1,24 @@
+package java_pattern.pattern1;
+
+import java.util.Scanner;
+
+public class PrintPattern10 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		var n = sc.nextInt();
+		int i = 1;
+		while (i <= n) {
+			var j = 1;
+			while (j <= i) {
+				System.out.print(i+1-j);
+				j++;
+			}
+			System.out.println();
+			i++;
+
+		}
+
+	}
+
+}

d1e17b24e7c8da9593ec67ac988cf217d0ed489c
Pankaj Singh <p7ksingh@gmail.com>
2024-12-29 23:52:22 +0530

    Add files via upload

---
+++ PrintPattern20.java
@@ -0,0 +1,29 @@
+package java_pattern.pattern1;
+
+import java.util.Scanner;
+
+public class PrintPattern20 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		var n = sc.nextInt();
+		int i = 1;
+		while (i <= n) {
+			int j = 1;
+			while (j <= n - i) {
+				System.out.print(" ");
+				j++;
+			}
+			j = 1;
+			while (j <= 2 * i - 1) {
+				System.out.print("*");
+				j++;
+			}
+
+			System.out.println();
+			i++;
+		}
+
+	}
+
+}
